75da543a549facfe594c9ac1517d1e0338670913
Chetan Shukla <193-chetan_shukla@users.noreply.newton.msvc.wisc.edu>
2021-04-27 22:59:18 -0500

    Using point.hpp and point.cpp files for interactions with the Point class

---
+++ EdgeDetection/point.cpp
@@ -0,0 +1,17 @@
+#include "global.hpp"
+#include "hashmap.hpp"
+
+using namespace std;
+
+// Point constructor
+Point::Point(int a, int b)
+{
+	x = a;
+	y = b;
+}
+
+Point::Point()
+{
+	x = 0;
+	y = 0;
+}
---
+++ EdgeDetection/point.hpp
@@ -0,0 +1,14 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+// Point class that will be used in the HashMap
+class Point
+{
+    public:
+    	int x, y;
+	    Point(int, int);
+	    Point(void);
+};
+
+
+#endif
