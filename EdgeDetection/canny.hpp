8f4353b2bf9a59bc9f230af29b3cbd9dcd3b9fb9
Chetan Shukla <193-chetan_shukla@users.noreply.newton.msvc.wisc.edu>
2021-05-01 12:25:45 -0500

    Changed the canny.hpp header file to exclude the convolution method

--- EdgeDetection/canny.hpp
+++ EdgeDetection/canny.hpp
@@ -7,10 +7,9 @@
 
 using namespace std;
 
-void magnitude_matrix(double **pic, double **mag, double **x, double **y);
-
 std::vector<Point*> peak_detection(double **mag, unordered_map<Point*, bool> peaks, double **x, double **y);
 
-void recursiveDT(double **mag, double **final, unordered_map<Point*, bool> h, unordered_map<Point*, bool> peaks, int i, int j, int flag);
+void recursiveDoubleThresholding(double **mag, double **final, unordered_map<Point*, bool> visited, 
+                                    unordered_map<Point*, bool> peaks, int i, int j, int flag);
 
 #endif
