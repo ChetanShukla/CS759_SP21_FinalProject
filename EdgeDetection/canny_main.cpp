f46399a1bb0ac27b704faa94db053fc44c9f7afd
Chetan Shukla <193-chetan_shukla@users.noreply.newton.msvc.wisc.edu>
2021-05-02 19:54:02 -0500

    Using stb_image header files to read PNG images which are used for further processing

--- EdgeDetection/canny_main.cpp
+++ EdgeDetection/canny_main.cpp
@@ -1,13 +1,15 @@
 #include "canny.hpp"
 #include "global.hpp"
-#include "point.hpp"
+#include "pixel.hpp"
 #include "canny.cuh"
+#include <cuda.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 using namespace std;
 
 char type[10];
-int height;
-int width;
 int intensity;
 int hi;
 int lo;
@@ -21,6 +23,56 @@ int main(int argc, char **argv)
 		cout << "Proper syntax: ./a.out <input_filename> <high_threshold> <sigma_value>" << endl;
 		return 0;
 	}
+
+    int width, height, channels, total_images = 100;
+
+    for (int image_count = 1; image_count <= total_images; image_count++) {
+        string filename = "image-" + to_string(image_count) + ".png";
+        string path = "./processed_images/" + filename;
+
+        cout << "\nProcessing Image: " << path << "\n\n";
+
+        unsigned char *img = stbi_load(path.c_str(), &width, &height, &channels, 0);
+        if(img == NULL) {
+            printf("Error in loading the image\n");
+            exit(1);
+        }
+        printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
+
+        size_t gray_img_size = width * height * channels;
+        size_t img_size = width * height * channels;
+        int gray_channels = channels;
+        unsigned char *gray_img = (unsigned char*) malloc(gray_img_size);
+        unsigned int *pixels = (unsigned int*) malloc(gray_img_size);
+
+        unsigned int i = 0, j = 0;
+        for(unsigned char *p = img, *pg = gray_img; p != img + img_size; p += channels, pg += gray_channels) {
+            // *pg = (uint8_t)((*p + *(p + 1) + *(p + 2))/3.0);
+            *pg = (uint8_t) *p;
+            printf("%u ", *pg);
+
+            *(pixels + i) = (uint8_t) *pg;
+            i++;
+            
+            /* if(channels == 4) {
+                *(pg + 1) = *(p + 3);
+            } */
+        }
+        printf("\n\nLet the magic begin!\n");
+
+        for (i=0; i<height; i++) {
+            for (j=0; j<width; j++) {
+                printf("%u ", pixels[i*height + j]);
+            }
+            printf("\n");
+        }
+
+        // stbi_write_png("sky.png", width, height, channels, img, width * channels);
+        // stbi_write_jpg("sky2.jpg", width, height, channels, img, 100);
+
+        stbi_image_free(img);
+
+    }
 
 	// Exit program if file doesn't open
 	string filename(argv[1]);
@@ -42,7 +94,7 @@ int main(int argc, char **argv)
 	::sig = stoi(argv[3]);
 
 	// Storing header information and copying into the new ouput images
-	infile >> ::type >> ::width >> ::height >> ::intensity;
+	infile >> ::type >> width >> height >> ::intensity;
 	img1 << type << endl << width << " " << height << endl << intensity << endl;
 	img2 << type << endl << width << " " << height << endl << intensity << endl;
 	img3 << type << endl << width << " " << height << endl << intensity << endl;
@@ -72,13 +124,13 @@ int main(int argc, char **argv)
 	// magnitude_matrix(pic, mag, x, y);
 
 	// Get all the peaks and store them in vector
-	unordered_map<Point*, bool> peaks;
-	vector<Point*> v = peak_detection(mag, peaks, x, y);
+	unordered_map<Pixel*, bool> peaks;
+	vector<Pixel*> v = peak_detection(mag, peaks, x, y);
 
 	// Go through the vector and call the recursive function and each point. If the value
 	// in the mag matrix is hi, then immediately accept it in final. If lo, then immediately
 	// reject. If between lo and hi, then check if it's next to a hi pixel using recursion
-	unordered_map<Point*, bool>  h;
+	unordered_map<Pixel*, bool>  visited;
 	int a, b;
 	for (int i = 0; i < v.size(); i++)
 	{
@@ -90,7 +142,7 @@ int main(int argc, char **argv)
 		else if (mag[a][b] < lo)
 			final[a][b] = 0;
 		else
-			recursiveDoubleThresholding(mag, final, h, peaks, a, b, 0);
+			recursiveDoubleThresholding(mag, final, visited, peaks, a, b, 0);
 	}
 
 	// ================================= IMAGE OUTPUT =================================
