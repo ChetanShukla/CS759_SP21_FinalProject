684b8c6f5d01396308dbca7101497cb70a8c3056
Chetan Shukla <193-chetan_shukla@users.noreply.newton.msvc.wisc.edu>
2021-05-03 14:18:07 -0500

    Removed global.hpp from canny.hpp as it is no longer needed

--- EdgeDetection/canny.hpp
+++ EdgeDetection/canny.hpp
@@ -1,15 +1,17 @@
 #ifndef CANNY_HPP
 #define CANNY_HPP
 
-#include "point.hpp"
-#include "global.hpp"
+#include "pixel.hpp"
 #include <unordered_map>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
-std::vector<Point*> peak_detection(double **mag, unordered_map<Point*, bool> peaks, double **x, double **y);
+std::vector<Pixel*> peak_detection(double *mag, unordered_map<Pixel*, bool> peaks, double *x, double *y, 
+                                    const int height, const int width);
 
-void recursiveDoubleThresholding(double **mag, double **final, unordered_map<Point*, bool> visited, 
-                                    unordered_map<Point*, bool> peaks, int i, int j, int flag);
+void recursiveDoubleThresholding(double *mag, double *final, unordered_map<Pixel*, bool> visited, 
+                                    unordered_map<Pixel*, bool> peaks, int i, int j, int flag, const int width, const int height);
 
 #endif
