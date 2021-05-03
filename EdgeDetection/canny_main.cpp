854d8f36c83c208357288747862f39291de97933
Chetan Shukla <193-chetan_shukla@users.noreply.newton.msvc.wisc.edu>
2021-05-03 14:22:33 -0500

    Modified the canny_main.cpp to include all the steps required for canny edge detection

--- EdgeDetection/canny_main.cpp
+++ EdgeDetection/canny_main.cpp
@@ -1,8 +1,10 @@
 #include "canny.hpp"
-#include "global.hpp"
+// #include "global.hpp"
 #include "pixel.hpp"
 #include "canny.cuh"
 #include <cuda.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -11,9 +13,18 @@ using namespace std;
 
 char type[10];
 int intensity;
-int hi;
-int lo;
-double sig;
+
+#define BLOCK_SIZE 32
+#define WA 256   
+#define HA 256     
+#define HC 7    
+#define WC 7
+#define WB (WA - WC + 1)
+#define HB (HA - HC + 1)
+
+// int hi;
+// int lo;
+// double sig;
 
 void prepare_mask_arrays(float* maskx, float* masky, size_t dimension, int sigma) {
 
@@ -54,6 +65,25 @@ float* getPixelsFromPngImage(unsigned char *img, int width, int height, int chan
     return pixels;  
 }
 
+void getNormalisedMagnitudeMatrix(float* mag, unsigned int height, unsigned int width) {
+
+    float maxVal = 0f;
+	for (int i = 0; i < height; i++) {
+		for(int j = 0; j < width; j++) {
+            if (mag[i * width + j] > maxVal) {
+                maxVal = mag[i * width + j];     
+            }
+		}
+	}
+
+	// Make sure all the magnitude values are between 0-255
+    for (int i = 0; i < height; i++)
+		for (int j = 0; j < width; j++)
+            mag[i * width + j] = mag[i * width + j] / maxVal * 255;
+            
+    return mag;        
+}
+
 int main(int argc, char **argv)
 {
 	// Exit program if proper arguments are not provided by user
@@ -63,7 +93,11 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
+    double sig = 1;
     int dim = 6 * sig + 1, cent = dim / 2;
+
+    const int hi = 50;
+    const int lo = .35 * hi; 
 
     cudaError_t error;
     unsigned int mask_size = dim * dim;
@@ -73,6 +107,7 @@ int main(int argc, char **argv)
     float* maskx = (float*)malloc(mask_memory_size);
     float* masky = (float*)malloc(mask_memory_size);
 
+    // Step 1: Creation of mask arrays using the Gaussian derivative formula
     prepare_mask_arrays(maskx, masky, dim, sig);
 
     float* dev_mask_x;
@@ -127,6 +162,7 @@ int main(int argc, char **argv)
         }
         // printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
 
+        // Step 2 : PNG image to 2D matrix
         float *pixels = getPixelsFromPngImage(img, width, height, channels);
         unsigned int i = 0, j = 0; 
         
@@ -186,7 +222,20 @@ int main(int argc, char **argv)
             return EXIT_FAILURE;
         }    
 
-        convolve(pixels, dev_x, dev_y, dev_mask_x, dev_mask_y, height, width, height, width, dim); 
+        // Step 3: Convolution of the image matrix with the mask arrays in the two dimensions
+
+        dim3 threads(BLOCK_SIZE, BLOCK_SIZE);
+	    dim3 grid((WB - 1) / (BLOCK_SIZE - WC + 1), (WB - 1) / (BLOCK_SIZE - WC + 1));
+
+	    cudaEventRecord(start_G);
+        convolution_kernel <<< grid, threads >>> (dev_pixels, dev_x, dev_mask_x, height, width, height, width, maskDimension);
+        cudaEventRecord(stop_G);
+
+	    cudaEventRecord(start_G);
+	    convolution_kernel <<< grid, threads >>> (dev_pixels, dev_y, dev_mask_y, height, width, height, width, maskDimension);
+        cudaEventRecord(stop_G);
+
+        // convolve(pixels, dev_x, dev_y, dev_mask_x, dev_mask_y, height, width, height, width, dim); 
 
         // After this step, we'll get the convolution in x direction and y direction in
         // the arrays x and y, which would later be used to generate vector of peaks
@@ -206,6 +255,80 @@ int main(int argc, char **argv)
             return EXIT_FAILURE;
         }  
 
+        float* mag = new float[height*width];
+
+        float* dev_mag;
+
+        // allocate image memory on the device(GPU)
+		error = cudaMalloc((void**)&dev_mag, sizeof(float) * height * width);
+		if (error != cudaSuccess) {
+			printf("device image memory allocation failed");
+			return EXIT_FAILURE;
+		}
+
+        const int threads_per_block = 256;
+		int num_blocks = (img_size + threads_per_block - 1) / threads_per_block;
+
+        // Step 4: Get the magnitude matrix using the x[] and y[] that we got from the previous step
+
+        magnitude_matrix_kernel <<<num_blocks, threads_per_block>>> (dev_mag, dev_x, dev_y, height, width);
+
+        // Copy back the contents of dev_mag to the host
+        error = cudaMemcpy(mag, dev_mag, sizeof(float) * height * width, cudaMemcpyDeviceToHost);
+        if (error != cudaSuccess) {
+            printf("Copying back mag[] to the host failed");
+            return EXIT_FAILURE;
+        }
+
+        getNormalisedMagnitudeMatrix(mag, height, width);  
+
+        // Step 5: Get all the peaks and store them in a vector
+        unordered_map<Pixel*, bool> peaks;
+        vector<Pixel*> vector_of_peaks = peak_detection(mag, peaks, x, y, height, width);
+
+
+        // Step 6: Creation of the final image matrix using the magnitude matrix and
+        // Recursive Double Thresholding
+        uint8_t* final = new uint8_t[img_size];
+
+        // Go through the vector and call the recursive function and each point. If the value
+        // in the mag matrix is hi, then immediately accept it in final. If lo, then immediately
+        // reject. If between lo and hi, then check if it's next to a hi pixel using recursion
+        unordered_map<Pixel*, bool>  visited;
+        int a, b;
+        for (int i = 0; i < vector_of_peaks.size(); i++)
+        {
+            a = vector_of_peaks.at(i)->x;
+            b = vector_of_peaks.at(i)->y;
+
+            if (mag[a * width + b] >= hi)
+                final[a * width + b] = 255;
+            else if (mag[a * width + b] < lo)
+                final[a * width + b] = 0;
+            else
+                recursiveDoubleThresholding(mag, final, visited, peaks, a, b, 0, width, height, lo);
+        }
+
+        
+        // Final step : Keeping the final image matrix 
+
+        uint8_t* dev_final_image;
+
+        // allocate image memory on the device(GPU)
+		error = cudaMalloc((void**)&dev_final_image, sizeof(uint8_t) * img_size);
+		if (error != cudaSuccess) {
+			printf("device image memory allocation failed");
+			return EXIT_FAILURE;
+		}
+
+		// put the image on the device
+		error = cudaMemcpy(dev_final_image, final, sizeof(uint8_t) * img_size, cudaMemcpyHostToDevice);
+		if (error != cudaSuccess) {
+			printf("image move to device failed");
+			return EXIT_FAILURE;
+		}
+
+
         // stbi_write_png("sky.png", width, height, channels, img, width * channels);
         // stbi_write_jpg("sky2.jpg", width, height, channels, img, 100);
 
@@ -213,8 +336,11 @@ int main(int argc, char **argv)
 
         delete[] x;
         delete[] y;
+        delete[] final;
+        delete[] mag;
         cudaFree(dev_x);
         cudaFree(dev_y);
+        cudaFree(dev_final_image);
 
     }
     
@@ -238,22 +364,24 @@ int main(int argc, char **argv)
 	ofstream img2("./output_images/canny_peaks.pgm", ios::binary);		
 	ofstream img3("./output_images/canny_final.pgm", ios::binary);
 
-	::hi = stoi(argv[2]);
-	::lo = .35 * hi;
-	::sig = stoi(argv[3]);
+	// ::hi = stoi(argv[2]);
+	// ::lo = .35 * hi;
+	// ::sig = stoi(argv[3]);
 
 	// Storing header information and copying into the new ouput images
 	// infile >> ::type >> width >> height >> ::intensity;
-	img1 << type << endl << width << " " << height << endl << intensity << endl;
-	img2 << type << endl << width << " " << height << endl << intensity << endl;
-	img3 << type << endl << width << " " << height << endl << intensity << endl;
+	// img1 << type << endl << width << " " << height << endl << intensity << endl;
+	// img2 << type << endl << width << " " << height << endl << intensity << endl;
+	// img3 << type << endl << width << " " << height << endl << intensity << endl;
 
 	// These matrices will hold the integer values of the input image and masks.
 	// I'm dynamically allocating arrays to easily pass them into functions.
-	double **pic = new double*[height], **mag = new double*[height], **final = new double*[height];
+	/*
+    double **pic = new double*[height], **mag = new double*[height], **final = new double*[height];
 	double **x = new double*[height], **y = new double*[height];
 
-	for (int i = 0; i < height; i++)
+	
+    for (int i = 0; i < height; i++)
 	{
 		pic[i] = new double[width];
 		mag[i] = new double[width];
@@ -261,52 +389,36 @@ int main(int argc, char **argv)
 		x[i] = new double[width];
 		y[i] = new double[width];
 	}
+    */
 
 	// Reading in the input image as integers
-	for (int i = 0; i < height; i++)
+	/*
+    for (int i = 0; i < height; i++)
 		for (int j = 0; j < width; j++)
 			pic[i][j] = (int)infile.get();
 
     convolve(pic, x, y, maskx, masky, height, width, height, width);        
-
+    */
 	// Create the magniute matrix
 	// magnitude_matrix(pic, mag, x, y);
 
-	// Get all the peaks and store them in vector
-	unordered_map<Pixel*, bool> peaks;
-	vector<Pixel*> v = peak_detection(mag, peaks, x, y);
 
-	// Go through the vector and call the recursive function and each point. If the value
-	// in the mag matrix is hi, then immediately accept it in final. If lo, then immediately
-	// reject. If between lo and hi, then check if it's next to a hi pixel using recursion
-	unordered_map<Pixel*, bool>  visited;
-	int a, b;
-	for (int i = 0; i < v.size(); i++)
-	{
-		a = v.at(i)->x;
-		b = v.at(i)->y;
-
-		if (mag[a][b] >= hi)
-			final[a][b] = 255;
-		else if (mag[a][b] < lo)
-			final[a][b] = 0;
-		else
-			recursiveDoubleThresholding(mag, final, visited, peaks, a, b, 0);
-	}
+	
 
 	// ================================= IMAGE OUTPUT =================================
 	// Outputting the 'mag' matrix to img1. It's very important to cast it to a char.
 	// To make sure that the decimal doesn't produce any wonky results, cast to an int
 	// ================================= IMAGE OUTPUT =================================
-	for (int i = 0; i < height; i++)
+	/*
+    for (int i = 0; i < height; i++)
 		for (int j = 0; j < width; j++)
 			img1 << (char)((int)mag[i][j]);
 
 	// Outputting the points stored in the vector to img2
 	int k = 0;
-	for (int i = 0; i < v.size(); i++)
+	for (int i = 0; i < vector_of_peaks.size(); i++)
 	{
-		while(k++ != (v.at(i)->x * height + v.at(i)->y - 1))
+		while(k++ != (vector_of_peaks.at(i)->x * height + vector_of_peaks.at(i)->y - 1))
 			img2 << (char)(0);
 
 		img2 << (char)(255);
@@ -316,6 +428,6 @@ int main(int argc, char **argv)
 	for (int i = 0; i < height; i++)
 		for (int j = 0; j < width; j++)
 			img3 << (char)((int)final[i][j]);		
-
+    */
 	return 0;
 }
